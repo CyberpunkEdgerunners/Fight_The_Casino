e1ed28203c4cd880aca5fd2cb6e61787659c3853
Andrew Little <alittle8@csub.edu>
2024-04-27 14:45:44 -0700

    updated to influence money object

--- slotScript.js
+++ slotScript.js
@@ -1,41 +1,37 @@
 function slotMachines() {
-    let m = document.getElementById("money");
-    let money = parseInt(m.innerText.replace(/[\$,]/g, ""), 10); // Used to convert money to an int for calculations
+    const min = 100;
+    let playAgain = true;
 
-    const min = 10;
+    while (playAgain) {
 
-    if (money < min) {
-        alert("Sorry! You do not have enough money!");
-        return;
-    }
-
-    while (bet < 10 || bet > 1000 || isNaN(bet)) {
-        bet = parseInt(prompt("Enter your bet amount ($10 to $1000):"), 10);
-
-        if (isNaN(bet) || bet < 10 || bet > 1000) {
-            alert("Invalid bet amount. Please enter a value between $10 and $1000.");
+        if (money.get_balance() < min) {
+            alert("Sorry! You do not have enough money!");
+            return;
         }
-    }
 
-    money -= bet;
-    let slot1 = Math.floor(Math.random() * 3) + 1; // Random number beyween 1 and 3
-    let slot2 = Math.floor(Math.random() * 3) + 1; 
-    let slot3 = Math.floor(Math.random() * 3) + 1; 
+        let bet = 0;
+        while (bet < 10 || bet > 100 || isNaN(bet)) {
+            bet = parseInt(prompt("Enter your bet amount ($10 to $100):"), 10);
 
-    if (slot1 === slot2 && slot2 === slot3) {
-        money += bet * 4;
-        alert("Congratulations! You won");
-    } else {
-        alert("Sorry, you lost.");
-    }
+            if (isNaN(bet) || bet < 10 || bet > 100) {
+                alert("Invalid bet amount. Please enter a value between $10 and $100.");
+            }
+        }
 
-    money.innerText = "$" + currentMoney.toLocaleString();  // Update the displayed money (how to add comma?)
+        money.subMoney(bet);
+        let slot1 = Math.floor(Math.random() * 3) + 1; // Random number between 1 and 3
+        let slot2 = Math.floor(Math.random() * 3) + 1; 
+        let slot3 = Math.floor(Math.random() * 3) + 1; 
 
-    let playAgain = confirm("Play again?");
-    if (playAgain) {
-        slotMachines();
-    } else {
-        return
+        if (slot1 === slot2 && slot2 === slot3) {
+            money.addMoney(bet * 4);
+            alert("Congratulations! You won");
+        } else {
+            alert("Sorry, you lost.");
+        }
+
+        playAgain = confirm("Play again?");
     }
 }
+document.getElementById("slot").addEventListener("click", slotMachines);
 
