6d70147c82275d294ab4ff7828ba23cf13b580c7
jacobjosiah <158225135+jacobjosiah@users.noreply.github.com>
2024-05-01 16:51:27 -0700

    Update coinscript.js

--- coinscript.js
+++ coinscript.js
@@ -47,12 +47,12 @@ function playCoinToss() {
     //let result = Math.random() < 0.5 ? "Heads" : "Tails";
     let result = Math.random() < 0.5 ? 1 : 2;
     if(result == 1 && ans == "heads") {
-        alert("The result is heads!");
             money.addMoney(bet * 4);
+        alert("The result is heads!");
         // show the random value and update the money
     } else if (result == 2 && ans == "tails") {
+            money.addMoney(bet * 4);
         alert("The result is tails!");
-              money.addMoney(bet * 4);
         // show the random value and update the money
     }
 
