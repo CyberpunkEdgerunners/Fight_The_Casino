69879c4415453dc9f03ac754dab88c7896211cf7
jacobjosiah <158225135+jacobjosiah@users.noreply.github.com>
2024-05-01 19:12:42 -0700

    Update coinscript.js

--- coinscript.js
+++ coinscript.js
@@ -7,7 +7,7 @@ function playCoinToss() {
         alert("Sorry! You do not have enough money!");
         return;
     }
-    
+
     // Prompt for bet amount
     let bet = -1;
     while (bet < 10 || bet > 100 || isNaN(bet)) {
@@ -28,18 +28,23 @@ function playCoinToss() {
     do
     {
         ans = prompt("Select 'heads' or 'tails'");
+        console.log();
     }
-        while (ans !== "heads" && ans !== "tails" && ans !== null););
+        while (ans !== "heads" && ans !== "tails" && ans !== null);
+     console.log();
 
     // Generate the result of the coin toss
     const result = Math.random() < 0.5 ? "heads" : "tails";
+     console.log();
 
     // Compare the result with the user's choice and update balance
     if (result === ans)
     {
+         console.log();
         money.addMoney(bet * 2);
         alert("You won! The result is " + result + "! You doubled your bet!");
-    } else 
+    } 
+    else 
     {
         alert("You lost! The result is " + result + "! Better luck next time!");
     }
