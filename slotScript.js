aacf166e930e51242dd3cd9fd0390b45239d6cbf
Andrew Little <alittle8@csub.edu>
2024-05-03 01:17:30 -0700

    Added rough draft of ASCII Art

--- slotScript.js
+++ slotScript.js
@@ -1,47 +1,55 @@
 function slotMachines() {
-    const min = 10;
-    let playAgain = true;
+    const min = 1; // $1 minimum bet
+    let autoBet = false
+    const slots = ['\uD83C\uDF51', '\uD83C\uDF4B', '\uD83C\uDF52', '\uD83C\uDF49', '\u0037\uFE0F\u20E3']; // Unicode sequences which represent different emojis // orange, watermelon, seven, lemon
+    let bet = -1;
 
-    
-
-        if (money.getBalance() < min) {
-            alert("Sorry! You do not have enough money!");
-            return;
+    if (money.getBalance() < min) { // Exits function if user is too broke
+        alert("Sorry! You do not have enough money!");
+        return;
+    }
+ 
+    while (bet < 1 || bet > 100) { // While bet is not between $1 and $100
+        bet = prompt("Enter your bet amount ($1 to $100) \n\nLeave blank -> Auto Bet $50");
+        if (bet === "") { // Autobet is at $50 if user presses enter w/out typing a value
+            bet = 50;
         }
 
-        let bet = -1;
-        while (bet < 10 || bet > 100 || isNaN(bet)) {
-            bet = parseInt(prompt("Enter your bet amount ($10 to $100):"), 10);
-
-            if ((isNaN(bet) || bet < 10 || bet > 100) && bet != 0) {
-                alert("Invalid bet amount. Please enter a value between $10 and $100.");
+        else {
+            bet = parseInt(bet, 10)
+            if ((isNaN(bet) || bet < 1 || bet > 100) && bet != 0) {
+                alert("Invalid bet amount. Please enter a value between $1 and $100.");
             }
 
-            if (bet === 0) {
+            if (bet === 0 || isNaN(bet)) {
                 return;
             }
         }
+    }
 
-        money.subMoney(bet);
-        updateDisplay();
+    money.subMoney(bet);
+    updateDisplay();
 
-        let slot1 = Math.floor(Math.random() * 5) + 1; // Random number between 1 and 5
-        let slot2 = Math.floor(Math.random() * 5) + 1; 
-        let slot3 = Math.floor(Math.random() * 5) + 1; 
+    let slot1 = Math.floor(Math.random() * 5) + 1; // Random number between 1 and 5
+    let slot2 = Math.floor(Math.random() * 5) + 1;
+    let slot3 = Math.floor(Math.random() * 5) + 1;
 
-        if (slot1 === slot2 && slot2 === slot3) {
-            money.addMoney(bet * 4);
-            alert("Congratulations! You won the Jackpot!");
+    if (slot1 === slot2 && slot2 === slot3 && slot3 === 5) {
+        money.addMoney(bet * 6);
+        alert("+--------------------------------------+\n|             SUPER SLOTS             |\n|--------------------------------------|\n|\t\t\t\t      |\n|     +-------+  +-------+  +-------+  |\n|     | " + slots[4]+ "  |  | " + slots[4]+ "  |  | " + slots[4]+ " |  |\n|     +-------+  +-------+  +-------+  |\n|     | " + slots[4]+ "  |  | " + slots[4]+ "  |  | " + slots[4]+ " |  |\n|     +-------+  +-------+  +-------+  |\n|\t\t\t\t      |\n|\t\t\t\t      |\n|            [   JACKPOT !!  ]           |\n|\t\t\t\t      |\n+--------------------------------------+");
 
-        } else if ((slot1 === slot2) || (slot1 === slot3) || (slot2 === slot3)) {
-            money.addMoney(bet * 2);
-            alert("Congratulations! You got a matching pair!");
-            
-        } else {
-            alert("Sorry, you lost.");
-        }
+    } else if ((slot1 === slot2 && slot2 === slot3)) {
+        money.addMoney(bet * 3);
+        alert("+--------------------------------------+\n|             SUPER SLOTS             |\n|--------------------------------------|\n|\t\t\t\t      |\n|     +-------+  +-------+  +-------+  |\n|     | " + slots[slot1 - 1]+ "  |  | " + slots[slot1 - 1]+ "  |  | " + slots[slot1 - 1]+ " |  |\n|     +-------+  +-------+  +-------+  |\n|     | " + slots[slot1 - 1]+ "  |  | " + slots[slot1 - 1]+ "  |  | " + slots[slot1 - 1]+ " |  |\n|     +-------+  +-------+  +-------+  |\n|\t\t\t\t      |\n|\t\t\t\t      |\n|            [   TOUGH TRIO !!  ]           |\n|\t\t\t\t      |\n+--------------------------------------+");
 
-        updateDisplay();
-        check();      
+    } else if ((slot1 === slot2) || (slot1 === slot3) || (slot2 === slot3)) {
+        money.addMoney(bet * 2);
+        alert("+--------------------------------------+\n|             SUPER SLOTS             |\n|--------------------------------------|\n|\t\t\t\t      |\n|     +-------+  +-------+  +-------+  |\n|     | " + slots[slot1 - 1]+ "  |  | " + slots[slot2 - 1]+ "  |  | " + slots[slot3 - 1]+ " |  |\n|     +-------+  +-------+  +-------+  |\n|     | " + slots[slot1 - 1]+ "  |  | " + slots[slot2 - 1]+ "  |  | " + slots[slot3 - 1]+ " |  |\n|     +-------+  +-------+  +-------+  |\n|\t\t\t\t      |\n|\t\t\t\t      |\n|            [   DOUBLES !!  ]           |\n|\t\t\t\t      |\n+--------------------------------------+");
+
+    } else {
+        alert("+--------------------------------------+\n|             SUPER SLOTS             |\n|--------------------------------------|\n|\t\t\t\t      |\n|     +-------+  +-------+  +-------+  |\n|     | " + slots[slot1 - 1]+ "  |  | " + slots[slot2 - 1]+ "  |  | " + slots[slot3 - 1]+ " |  |\n|     +-------+  +-------+  +-------+  |\n|     | " + slots[slot1 - 1]+ "  |  | " + slots[slot2 - 1]+ "  |  | " + slots[slot3 - 1]+ " |  |\n|     +-------+  +-------+  +-------+  |\n|\t\t\t\t      |\n|\t\t\t\t      |\n|            [   FAILURE !!  ]           |\n|\t\t\t\t      |\n+--------------------------------------+");
+    }
+
+    updateDisplay();
 }
 document.getElementById("slot").addEventListener("click", slotMachines);
