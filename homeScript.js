58d33c1fab277e33bdba3d511e9d0f1430b3719b
Andy Ceballos <aceballos@odin.cs.csubak.edu>
2024-05-04 17:28:46 -0700

    added cookie functionality to save username

--- homeScript.js
+++ homeScript.js
@@ -3,9 +3,30 @@ let pee = document.getElementById("pee");
 let storyPic = document.getElementById("imgD");
 
 
-let name = prompt("Please enter your name");
-document.getElementById("nickName").innerHTML += name;
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
 
+//ask the user only once for the name and will save the info for next time
+//clear cookies from history in order to enter a new name
+let nameFlag = getCookie("name");
+if(!nameFlag) {
+    let name = prompt("Please enter your name");
+    document.cookie = "name=" + name;
+}
+document.getElementById("nickName").innerHTML += getCookie("name");
 
 // id = dialog makes a pop up dialog
 // the #id has to be a div element 
@@ -14,19 +35,37 @@ $( function() {
     $( "#dialog" ).dialog();
 } );
 
+
 // id = dialog makes the height and width of the pop up
 $( "#dialog" ).dialog({
-  width: 500,
-  height: 500,
+    width: 500,
+    height: 500,
 });
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+} 
+
 
 
 
 let lineCounter = 1;
 const settingMess = "King Nero was the richest and most powerful human to ever lay foot on planet earth, however one man stood in front of him ready to risk it all.";
 let   kingTalkOne = "Welcome ";
-      kingTalkOne += name;
-      kingTalkOne += ", I've heard great tales of your adventure but I don't think you're ready to Fight The Casino";
+kingTalkOne += name;
+kingTalkOne += ", I've heard great tales of your adventure but I don't think you're ready to Fight The Casino";
 const playerTalkOne = "I've spent decades honing my strength to one day take you down and rightfully take what's mine, brother";
 const kingTalkTwo = "If you can earn enough money from the casino then I'll enligthen you but currently I dont have time to spend on trash";
 const playerTalkTwo = "I'll destory everything here until its only just me and you";
@@ -37,28 +76,28 @@ function messages(lineCounter) {
     if(lineCounter == 1) { 
         storyPic.src = "https://i.imgur.com/hNCqe6H.png";
         return settingMess;
-     }else if(lineCounter == 2) {
+    }else if(lineCounter == 2) {
         storyPic.src = "https://i.imgur.com/vLmXVUC.png";
         return kingTalkOne;
-     }else if(lineCounter == 3) {
+    }else if(lineCounter == 3) {
         storyPic.src = "https://i.imgur.com/npnRwbJ.png";
         return playerTalkOne;
-     }else if(lineCounter == 4) {
+    }else if(lineCounter == 4) {
         storyPic.src = "https://i.imgur.com/vLmXVUC.png";
         return kingTalkTwo;
-     }else if(lineCounter == 5) {
+    }else if(lineCounter == 5) {
         storyPic.src = "https://i.imgur.com/npnRwbJ.png";
         return playerTalkTwo;
-     }else if(lineCounter == 6) {
+    }else if(lineCounter == 6) {
         storyPic.src = "https://i.imgur.com/vLmXVUC.png";
         return kingTalkThree;
-     }else 
+    }else 
         return -1;
 }
 
 window.addEventListener("load", function(){
 
-     story.addEventListener("click", function() {
+    story.addEventListener("click", function() {
         let ret = messages(lineCounter);
         lineCounter++;
         if(ret == -1) {
